c2ea0c696ad0fcd3eb1201731251b3302f64a35b
Joao Vicente <joao.diogo.vicente@gmail.com>
2017-03-03 01:19:01 +0000

    Introduced support for time modifiers (e.g. -1d) in timeline

--- app/tracy-core/tracy-modules/task-timeline/taskTimelineController.js
+++ app/tracy-core/tracy-modules/task-timeline/taskTimelineController.js
@@ -24,8 +24,8 @@ function TaskTimelineController
       + "?env=" + $scope.environment
       + "&application=" + $scope.application
       + "&task=" + $scope.task
-      + "&earliest=" + $stateParams['earliest']
-      + "&latest=" + $stateParams['latest']
+      + "&earliest=" + $scope.earliest
+      + "&latest=" + $scope.latest
       + "&rtBelow=" + $stateParams['rtBelow']
       + "&rtAbove=" + $stateParams['rtAbove'];
     return prevUrl;
@@ -41,8 +41,8 @@ function TaskTimelineController
     + "?env=" + $scope.environment
     + "&application=" + $scope.application
     + "&task=" + $scope.task
-    + "&earliest=" + $stateParams['earliest']
-    + "&latest=" + $stateParams['latest']
+    + "&earliest=" + $scope.earliest
+    + "&latest=" + $scope.latest
     + "&rtBelow=" + $stateParams['rtBelow']
     + "&rtAbove=" + $stateParams['rtAbove'];
     return nextUrl;
@@ -81,11 +81,6 @@ function TaskTimelineController
 
   //TaskMeasurement.get({application: $scope.application, task: $scope.task},
   function mockTaskAnalysisReource(query) {
-    // GET .../tws/v1/applications/Direct+/tasks/IDR-Golf/analysis
-    // &filter=msecBefore:[1446187500000 TO 1446201000000] AND msecElapsed:[120 TO 180] AND t_application:”Direct+" AND t_task:”IDR-Golf"
-    // ?sort=-msecElapsed
-    // &offset=0
-    // &limit=20
     var response = {application: '', task: '', tracyTasksPage : {}};
     response.application = query.application;
     response.task = query.task;
@@ -116,9 +111,92 @@ function TaskTimelineController
           $scope.disableLast = disableIfLast();
           $scope.prevUrl = previousUrl();
           $scope.nextUrl = nextUrl();
-          deferred.resolve(response.tracyTasksPage.tracyTasks[$scope.sequenceId-1].tracyTask.tracyEvents);
+
+          var selectedSequenceId;
+          if ($scope.sequenceId == "_last") {
+            // FIXME: _last is a hack to display the last frame in the time window
+            $scope.sequenceId = 1;
+            $scope.lastId = 1;
+            deferred.resolve(response.tracyTasksPage.tracyTasks[response.tracyTasksPage.records-1].tracyTask.tracyEvents);
+          }
+          else  {
+            deferred.resolve(response.tracyTasksPage.tracyTasks[$scope.sequenceId-1].tracyTask.tracyEvents);
+          }
         });
         return deferred.promise;
+  }
+
+  function isRelativeTimeModifier(modifier) {
+    var qualifierSplit = modifier.split(/(\d)/);
+    var test = qualifierSplit[0]=="-"
+        && !isNaN(qualifierSplit[1])
+        && (qualifierSplit[2] == "d"
+            || qualifierSplit[2] == "h"
+            || qualifierSplit[2] == "m"
+            || qualifierSplit[2] == "s")
+    if (test)   {
+//        console.log(modifier + " is a valid modifier")
+    }
+    else    {
+        console.log(modifier + " is NOT a valid modifier")
+    }
+    return test;
+  }
+
+  function getTimeOffset(timeQualifier)  {
+    var qualifierSplit = timeQualifier.split(/(\d)/);
+    var timeOffset = 0;
+
+     if (qualifierSplit[2] == "d")  {
+       timeOffset = Date.now() - qualifierSplit[1]*(1000*60*60*24)
+     }
+     else if(qualifierSplit[2] == "h")  {
+       timeOffset = Date.now() - qualifierSplit[1]*(1000*60*60)
+     }
+     else if(qualifierSplit[2] == "m")  {
+       timeOffset = Date.now() - qualifierSplit[1]*(1000*60)
+     }
+     else if(qualifierSplit[2] == "s")  {
+       timeOffset = Date.now() - qualifierSplit[1]*(1000)
+     }
+     else {
+        console.log("Unknown time qualifier:" + timeQualifier);
+     }
+    return timeOffset;
+  }
+
+  function timeModifierConversion(timeQualifier)   {
+    var time=0;
+    if (isNaN(timeQualifier))   {
+//      console.log(timeQualifier);
+      if (timeQualifier == "now") {
+        time = Date.now();
+      }
+      else if (isRelativeTimeModifier(timeQualifier)) {
+        time = getTimeOffset(timeQualifier)
+      }
+    }
+    else {
+      time = timeQualifier;
+    }
+    return time;
+  }
+
+  function getSequenceIdFromKeyword(keyword)  {
+    var id;
+//    console.log("$scope.lastId: " + $scope.lastId);
+    if (keyword == "_first") {
+      id = 1;
+    }
+    else if (keyword == "_last") {
+      // FIXME: _last requires knowing how many will be returned in advance
+      id = keyword;
+    }
+    else {
+      id = keyword;
+    }
+//    console.log(id);
+    return id;
   }
 
   function prepareAnalysisQuery(application, task, earliest, latest, rtAbove, rtBelow) {
@@ -141,16 +219,21 @@ function TaskTimelineController
   var offset = 3601000; // hourOffset
 
   $scope.environment = $stateParams['env'];
-  $scope.earliest = new Date(Number($stateParams['earliest'])).toUTCString();
-  $scope.latest = (new Date(Number($stateParams['latest']))).toUTCString();
+  // Convert time modifiers to epoch time
+  $scope.earliest = timeModifierConversion($stateParams['earliest']);
+  $scope.latest = timeModifierConversion($stateParams['latest']);
   $scope.rtAbove = $stateParams['rtAbove'];
   $scope.rtBelow = $stateParams['rtBelow'];
-  $scope.sequenceId = $stateParams['sequenceId'];
+  $scope.sequenceId = getSequenceIdFromKeyword($stateParams['sequenceId'])
   $scope.application = (typeof $stateParams['application'] === 'undefined') ? 'unknown-application' : $stateParams['application'];
   $scope.task = (typeof $stateParams['task'] === 'undefined') ? 'unknown-task' : $stateParams['task'];
 
+//  console.log("earliest=" + $stateParams['earliest'] + ", latest=" + $stateParams['latest']);
+//  console.log("earliest=" + $scope.earliest + ", latest=" + $scope.latest);
+//  console.log($stateParams);
+
   Storage.setSelectedEnvironment($scope.environment);
-  $scope.query = prepareAnalysisQuery($scope.application, $scope.task, $stateParams['earliest'], $stateParams['latest'], $scope.rtAbove, $scope.rtBelow);
+  $scope.query = prepareAnalysisQuery($scope.application, $scope.task, $scope.earliest, $scope.latest, $scope.rtAbove, $scope.rtBelow);
   $scope.dataPromise = $scope.getTaskAnalysis($scope.query);
 //  console.log(JSON.stringify($scope.dataPromise));
 //  $scope.dataPromise.then(function(greeting) {
